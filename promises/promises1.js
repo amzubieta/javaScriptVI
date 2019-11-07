4820133986e7b4122845b9fee30475ad312bc754
Angelica Zubieta <angelicazubieta@gmail.com>
2019-11-07 11:07:07 -0300

    then y catch

---
+++ promises/promises1.js
@@ -0,0 +1,14 @@
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject)=>{
+        if (segundos>10) {
+            reject("Muito tempo para esperar")
+        }
+        setTimeout(() => {
+           resolve(frase) 
+        }, segundos*1000);
+    })
+}
+
+falarDepoisDe(3, "Hm, esperei para falar").then(frase=>console.log(`${frase} - vim da promise`)).catch(e=>console.log(e))
+
+
